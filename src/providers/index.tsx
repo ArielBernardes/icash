b15859e22f1a4f7b0804728e9b1122614cf92728
luizrafa-federico <luizrafa.federico@gmail.com>
2021-09-07 09:56:34 -0500

    refactor: index.tsx Providers into TS

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,12 +1,16 @@
 import { ReactNode } from "react";
+import { AuthProvider } from "./Auth";
+import { RegisterProvider } from "./Register";
 
 interface ProvidersProps {
-    children: ReactNode;
+  children: ReactNode;
 }
-export const Providers = ({children}: ProvidersProps) => {
-    return (
-        <>
-        </>
-    )
-
-}
+export const Providers = ({ children }: ProvidersProps) => {
+  return (
+    <>
+      <AuthProvider>
+        <RegisterProvider>{children}</RegisterProvider>
+      </AuthProvider>
+    </>
+  );
+};
