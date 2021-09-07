6bcfdb18e0e982cd9bd2238f53fb17b5234dd7fb
luizrafa-federico <luizrafa.federico@gmail.com>
2021-09-07 04:27:13 -0500

    refactor: HomeIntro component into TS

---
+++ src/components/HomeIntro/index.tsx
@@ -0,0 +1,37 @@
+import { LoggingInPaths } from "./styles";
+import Image from "../../components/Image";
+import ArrowLeft from "../../assets/arrowLeft.svg";
+import ArrowRight from "../../assets/arrowRight.svg";
+import { useHistory } from "react-router-dom";
+
+const HomeIntro = () => {
+  const history = useHistory();
+
+  const handleNavigation = (path: string) => {
+    history.push(path);
+  };
+  return (
+    <LoggingInPaths>
+      <div onClick={() => handleNavigation("/login")} className="loginWrapper">
+        <span>Já tenho conta</span>
+        <Image src={ArrowRight} alt="logo-to-login" />
+      </div>
+      <div
+        onClick={() => handleNavigation("/signup")}
+        className="accountWrapper"
+      >
+        <Image src={ArrowLeft} alt="arrow-to-signUp" />
+        <span>Quero abrir minha conta</span>
+      </div>
+      <div
+        onClick={() => handleNavigation("/loyalty")}
+        className="loginWrapper"
+      >
+        <span>Ganhe 50 reais</span>
+        <Image src={ArrowRight} alt="logo-to-login" />
+      </div>
+    </LoggingInPaths>
+  );
+};
+
+export default HomeIntro;
