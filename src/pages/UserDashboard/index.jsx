f48c89f99a60152a1202899a03d36ebe4d05aabc
luizrafa-federico <luizrafa.federico@gmail.com>
2021-09-09 19:27:17 -0500

    style: mobile userDashboard

---
+++ src/pages/UserDashboard/index.jsx
@@ -0,0 +1,128 @@
+import { motion } from "framer-motion";
+import { CarouselWrapper } from "react-pretty-carousel";
+import {
+  DashboardPageWrapper,
+  Header,
+  SubHeader,
+  Stores,
+  Footer,
+} from "./styles";
+import SearchIcon from "../../assets/search.svg";
+import BellIcon from "../../assets/bell.svg";
+import FormLogo from "../../assets/LogoForm.svg";
+import { useState, useEffect } from "react";
+import WalletIcon from "../../assets/wallet.svg";
+import InformationIcon from "../../assets/information.svg";
+
+const stores = [
+  {
+    id: 1,
+    name: "Candid",
+    city: "São Paulo",
+    cashback: 15,
+    store_img:
+      "https://images.unsplash.com/photo-1564419965579-5da68ffdf3af?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+  },
+  {
+    id: 2,
+    name: "Home",
+    city: "São Paulo",
+    cashback: 25,
+    store_img:
+      "https://images.unsplash.com/photo-1535567679266-c113f99615bd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=656&q=80",
+  },
+  {
+    id: 3,
+    name: "Pharmacy",
+    city: "São Paulo",
+    cashback: 5,
+    store_img:
+      "https://images.unsplash.com/photo-1603706580932-6befcf7d8521?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+  },
+  {
+    id: 4,
+    name: "Magna Carta",
+    city: "São Paulo",
+    cashback: 35,
+    store_img:
+      "https://images.unsplash.com/photo-1506739738848-3da37ba381db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=564&q=80",
+  },
+  {
+    id: 5,
+    name: "Circo's",
+    city: "São Paulo",
+    cashback: 10,
+    store_img:
+      "https://images.unsplash.com/photo-1505066827145-34bcde228211?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=619&q=80",
+  },
+];
+
+const UserDashboard = () => {
+  const [items, setItems] = useState(3);
+
+  useEffect(() => {
+    if (window.innerWidth < 576) setItems(1);
+    else setItems(3);
+    window.addEventListener("resize", () => {
+      if (window.innerWidth < 576) setItems(1);
+      else setItems(3);
+    });
+  }, []);
+
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <DashboardPageWrapper>
+        <Header>
+          <div className="headerWrapper">
+            <div>
+              Olá, <span>Viviana</span>
+            </div>
+            <img src={BellIcon} alt="go-to-messages" />
+          </div>
+        </Header>
+        <SubHeader>
+          <figure className="imgLogo">
+            <img src={FormLogo} alt="icash-login-form" />
+          </figure>
+          <figure className="searchIcon">
+            <img src={SearchIcon} alt="find-stores" />
+          </figure>
+        </SubHeader>
+        <Stores>
+          <p>
+            Receba até <span>50%</span> de cashback
+          </p>
+          <CarouselWrapper items={items} mode="gallery" showControls={true}>
+            {stores.map((store, index) => (
+              <img
+                key={index}
+                className="image"
+                src={store.store_img}
+                alt={store.name}
+              />
+            ))}
+          </CarouselWrapper>
+        </Stores>
+        <Footer>
+          <div className="footerWrapper">
+            <img src={InformationIcon} alt="customer-service" />
+            <div className="infoWallet">
+              <img src={WalletIcon} alt="go-to-user-transactions" />
+              <span>R$ 134,50</span>
+            </div>
+            <figure className="profilePic">
+              <img src="https://i.pravatar.cc/150" alt="go-to-user-profile" />
+            </figure>
+          </div>
+        </Footer>
+      </DashboardPageWrapper>
+    </motion.div>
+  );
+};
+
+export default UserDashboard;
