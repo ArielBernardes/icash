5863c081444ff5df7f8a98bf5f5b3be9ebd37a42
Jorge Willian <jorgewillianpaez@gmail.com>
2021-09-10 16:30:18 -0300

    feat(card payment screen): buttons and style
    just to mobile for now

--- src/pages/PaymentWithCard/styles.ts
+++ src/pages/PaymentWithCard/styles.ts
@@ -1,7 +1,119 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100vh;
+`;
 
-export const Payment = styled.div``;
+export const Payment = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  background-color: var(--orangeCore);
+  height: 50%;
 
-export const PaymentOptions = styled.div``;
+  h3 {
+    color: var(--white);
+    margin-left: 140px;
+    margin-bottom: 16px;
+  }
+
+  p {
+    color: var(--white);
+    font-size: 12px;
+    text-align: center;
+    margin-bottom: 25px;
+    font-weight: 800;
+    width: 280px;
+  }
+`;
+
+export const Proof = styled.img`
+  position: absolute;
+  top: 50px;
+  left: 28px;
+`;
+
+export const PaymentOptions = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h2 {
+    color: var(--orangeCore);
+  }
+`;
+
+export const Options = styled.div`
+  display: flex;
+  margin-top: 30px;
+
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--lightOrange);
+    opacity: 1;
+    width: 110px;
+    height: 90px;
+    margin: 10px;
+    border-radius: 16px;
+    border: none;
+    color: var(--white);
+    :focus {
+      background-color: var(--orangeCore);
+    }
+
+    p {
+      font-size: 12px;
+    }
+  }
+
+  .wallet {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .cashback {
+    display: flex;
+    flex-direction: column;
+
+    h1 {
+      font-size: 32px;
+    }
+  }
+`;
+
+export const Value = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 30px;
+
+  div {
+    text-align: center;
+    color: var(--white);
+    background-color: var(--orangeCore);
+    width: 200px;
+    height: 30px;
+    border-radius: 20px;
+    margin-left: 10px;
+
+    p {
+      font-size: 20px;
+      margin-top: 5px;
+    }
+  }
+`;
+
+export const Pay = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--orangeCore);
+  height: 42px;
+  width: 240px;
+  border-radius: 20px;
+  border: none;
+  margin-top: 40px;
+  color: var(--white);
+`;
