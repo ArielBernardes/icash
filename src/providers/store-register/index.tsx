9e2cd3d348e68640d9bab5d5f9c0415bf598a5e9
luizrafa-federico <luizrafa.federico@gmail.com>
2021-09-11 12:23:34 -0500

    fix: bug on jwt-decode setting

--- src/providers/store-register/index.tsx
+++ src/providers/store-register/index.tsx
@@ -1,66 +1,68 @@
-import { createContext, ReactNode, useContext, useState } from "react"
-import toast from "react-hot-toast"
-import api from "../../services/api"
+import { createContext, ReactNode, useContext, useState } from "react";
+import toast from "react-hot-toast";
+import api from "../../services/api";
 import { IData } from "../../types/storeRegister";
 import { useAuth } from "../Auth";
 
 interface StoreRegisterProps {
-    children: ReactNode;
+  children: ReactNode;
 }
-
-
 
 interface StoreRegisterData {
-     storeRegister: (data: IData, setshowModalStore: React.Dispatch<React.SetStateAction<boolean>> ) => void;
-    showModalStore: boolean;
-    setShowModalStore: React.Dispatch<React.SetStateAction<boolean>>;
-    storeUpdate: (data: IData, setshowModalStore: React.Dispatch<React.SetStateAction<boolean>> ) => void;
+  storeRegister: (
+    data: IData,
+    setshowModalStore: React.Dispatch<React.SetStateAction<boolean>>
+  ) => void;
+  showModalStore: boolean;
+  setShowModalStore: React.Dispatch<React.SetStateAction<boolean>>;
+  storeUpdate: (
+    data: IData,
+    setshowModalStore: React.Dispatch<React.SetStateAction<boolean>>
+  ) => void;
 }
 
-const StoreRegisterContext = createContext<StoreRegisterData>({} as StoreRegisterData);
+const StoreRegisterContext = createContext<StoreRegisterData>(
+  {} as StoreRegisterData
+);
 
-export const StoreRegisterProvider = ({children}: StoreRegisterProps) => {
+export const StoreRegisterProvider = ({ children }: StoreRegisterProps) => {
+  const [showModalStore, setShowModalStore] = useState<boolean>(false);
 
-    const [showModalStore, setShowModalStore] = useState<boolean>(false);
+  const { token } = useAuth();
 
-    const {token } = useAuth();
-    console.log(token)
+  const storeRegister = (
+    data: IData,
+    setShowModalStore: React.Dispatch<React.SetStateAction<boolean>>
+  ) => {
+    api
+      .post("/stores", data, { headers: { Authorization: `Bearer ${token}` } })
+      .then(() => {
+        toast.success("Loja Cadastrada");
+        setShowModalStore(false);
+      })
+      .catch(() => toast.error("Erro ao cadastrar loja"));
+  };
 
-    const storeRegister = (data: IData , setShowModalStore: React.Dispatch<React.SetStateAction<boolean>>) => {
-        api
-        .post('/stores', data,
-         { headers: 
-            { Authorization: `Bearer ${token}`}
-        })
-            .then(() => {
-                toast.success('Loja Cadastrada');
-                setShowModalStore(false);
-            })
-            .catch(()=> toast.error('Erro ao cadastrar loja'))
-    }
+  const storeUpdate = (
+    data: IData,
+    setShowModalStore: React.Dispatch<React.SetStateAction<boolean>>
+  ) => {
+    api
+      .patch("/stores", data, { headers: { Authorization: `Bearer ${token}` } })
+      .then(() => {
+        toast.success("Dados atualizados");
+        setShowModalStore(false);
+      })
+      .catch(() => toast.error("Erro ao atualizar dados"));
+  };
 
-    const storeUpdate = (data: IData, setShowModalStore: React.Dispatch<React.SetStateAction<boolean>>) => {
-
-        api
-        .patch('/stores',data ,
-        { headers: 
-            {Authorization: `Bearer ${token}`}
-        })
-        .then(() => {
-            toast.success("Dados atualizados");
-            setShowModalStore(false);
-
-        })
-        .catch(()=> toast.error("Erro ao atualizar dados"))
-
-    }
-
-    return (
-        <StoreRegisterContext.Provider value = {{ storeUpdate, storeRegister, setShowModalStore, showModalStore }} >
-            {children}
-        </StoreRegisterContext.Provider>
-
-    )
-}
+  return (
+    <StoreRegisterContext.Provider
+      value={{ storeUpdate, storeRegister, setShowModalStore, showModalStore }}
+    >
+      {children}
+    </StoreRegisterContext.Provider>
+  );
+};
 
 export const useStoreRegister = () => useContext(StoreRegisterContext);
