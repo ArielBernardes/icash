e21018ed1fcda81a1021a62f701a8ca30714e606
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2021-09-16 14:50:39 -0300

    style: add transition effect between pages

--- src/pages/UserData/index.tsx
+++ src/pages/UserData/index.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { useHistory, Link } from "react-router-dom";
 import {
   ContainerUser,
@@ -16,24 +17,31 @@ const UserProfile = () => {
   const { name, email } = user;
 
   return (
-    <ContainerUser>
-      <HeaderUser />
-      <Container>
-        <Link to="/user-profile">
-          <img src={arrowLeft} alt="voltar" />
-        </Link>
-      </Container>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <ContainerUser>
+        <HeaderUser />
+        <Container>
+          <Link to="/user-profile">
+            <img src={arrowLeft} alt="voltar" />
+          </Link>
+        </Container>
 
-      <AvatarUser>
-        <img src="https://i.pravatar.cc/150" alt="perfil" />
-        <UserUpdateModal />
-      </AvatarUser>
+        <AvatarUser>
+          <img src="https://i.pravatar.cc/150" alt="perfil" />
+          <UserUpdateModal />
+        </AvatarUser>
 
-      <DataUser>
-        <p>Nome: {name}</p>
-        <p>E-mail: {email}</p>
-      </DataUser>
-    </ContainerUser>
+        <DataUser>
+          <p>Nome: {name}</p>
+          <p>E-mail: {email}</p>
+        </DataUser>
+      </ContainerUser>
+    </motion.div>
   );
 };
 
--- src/pages/UserProfile/index.tsx
+++ src/pages/UserProfile/index.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { useHistory, Link } from "react-router-dom";
 import {
   ContainerUser,
@@ -38,57 +39,64 @@ const UserProfile = () => {
     history.push("");
   };
   return (
-    <ContainerUser>
-      <HeaderUser />
-      <Container>
-        <Link to="/dashboard">
-          <img src={arrowLeft} alt="voltar" />
-        </Link>
-      </Container>
-      <ContainerAvatar>
-        <AvatarUser>
-          <img src="https://i.pravatar.cc/150" alt="perfil" />
-        </AvatarUser>
-        <BoxTitle>
-          <h3>
-            Olá, <span>{name}</span>{" "}
-          </h3>
-          <h4>{email}</h4>
-        </BoxTitle>
-      </ContainerAvatar>
-      <ContainerBtn>
-        <BtnProfileUser onClick={() => sendTo("/user-data")}>
-          Dados pessoais
-        </BtnProfileUser>
-        <BtnProfileUser onClick={() => sendTo("/credit-cards")}>
-          Cartão de crédito
-        </BtnProfileUser>
-        <BtnProfileUser onClick={() => sendTo("/posts")}>
-          Mensagens
-        </BtnProfileUser>
-        <BtnProfileUser onClick={() => sendTo("/icash")}>
-          Sobre ICash
-        </BtnProfileUser>
-      </ContainerBtn>
-      <FooterUser>
-        <span onClick={toggleModal}>Cancelar conta</span>
-        <span onClick={handleLogout}>Sair da sessão</span>
-      </FooterUser>
-      <Modal
-        isOpen={modalIsOpen}
-        ariaHideApp={false}
-        onRequestClose={toggleModal}
-        className="DeleteModal"
-      >
-        <ModalContent>
-          <p>Deseja mesmo cancelar a sua conta ICash?</p>
-          <ModalButtons>
-            <Button onClick={deleteAccount}>Sim</Button>
-            <Button onClick={toggleModal}>Não</Button>
-          </ModalButtons>
-        </ModalContent>
-      </Modal>
-    </ContainerUser>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <ContainerUser>
+        <HeaderUser />
+        <Container>
+          <Link to="/dashboard">
+            <img src={arrowLeft} alt="voltar" />
+          </Link>
+        </Container>
+        <ContainerAvatar>
+          <AvatarUser>
+            <img src="https://i.pravatar.cc/150" alt="perfil" />
+          </AvatarUser>
+          <BoxTitle>
+            <h3>
+              Olá, <span>{name}</span>{" "}
+            </h3>
+            <h4>{email}</h4>
+          </BoxTitle>
+        </ContainerAvatar>
+        <ContainerBtn>
+          <BtnProfileUser onClick={() => sendTo("/user-data")}>
+            Dados pessoais
+          </BtnProfileUser>
+          <BtnProfileUser onClick={() => sendTo("/credit-cards")}>
+            Cartão de crédito
+          </BtnProfileUser>
+          <BtnProfileUser onClick={() => sendTo("/posts")}>
+            Mensagens
+          </BtnProfileUser>
+          <BtnProfileUser onClick={() => sendTo("/icash")}>
+            Sobre ICash
+          </BtnProfileUser>
+        </ContainerBtn>
+        <FooterUser>
+          <span onClick={toggleModal}>Cancelar conta</span>
+          <span onClick={handleLogout}>Sair da sessão</span>
+        </FooterUser>
+        <Modal
+          isOpen={modalIsOpen}
+          ariaHideApp={false}
+          onRequestClose={toggleModal}
+          className="DeleteModal"
+        >
+          <ModalContent>
+            <p>Deseja mesmo cancelar a sua conta ICash?</p>
+            <ModalButtons>
+              <Button onClick={deleteAccount}>Sim</Button>
+              <Button onClick={toggleModal}>Não</Button>
+            </ModalButtons>
+          </ModalContent>
+        </Modal>
+      </ContainerUser>
+    </motion.div>
   );
 };
 
